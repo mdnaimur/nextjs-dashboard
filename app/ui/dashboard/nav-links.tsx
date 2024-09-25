acb201837732f543a13c978f5fcac7983e9741d2
mdnaimur <mdnaimurr@gmail.com>
2024-09-25 14:05:55 +0600

    bug fix

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,7 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
-export default function NavLinks({children}) {
+export default function NavLinks({}) {
   const pathname = usePathname();
   return (
     <>
